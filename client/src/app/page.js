0e0b60c419f7b5ebe44200c62f68b54a2a480bc6
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-05-18 19:09:57 +0600

    first commit

---
+++ client/src/app/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main page of our application.</p>
+      <p>Here you can find various features and functionalities.</p>
+    </div>
+  );
+};
+
+export default page;
