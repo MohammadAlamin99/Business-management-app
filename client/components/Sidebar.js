6c30d050c124e40b1bd51d48246bca13c63f47f9
alamin <alamintechlyn>
2025-05-19 14:39:32 +0600

    solved active sidebar problem

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,33 +1,41 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import { usePathname } from "next/navigation"; // Note: 'navigation' instead of 'router'
+import React from "react";
 import { GoHome } from "react-icons/go";
 import { IoIosArrowForward } from "react-icons/io";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
+
 const Sidebar = () => {
+  const pathname = usePathname();
 
-  const [isActive, setIsActive] = useState(false);
-
-  const handleactiveClick = () => {
-    setIsActive(!isActive);
-  }
   return (
     <div>
       <div className="sidebar_wrapper py-3.5 px-6">
         <div className="sidebar_menu">
           <h4 className="text-sm py-2 pl-1.5 uppercase">General</h4>
           <ul>
-            <li className={`menu_item`}>
-              <div className="menu_wrapper pl-2.5 pt-3 pb-3 pr-2.5">
-                <Link href={"/"} className={`flex items-center gap-2.5 text-base ${isActive ? 'navActive' : ''}`} onClick={handleactiveClick}>
+            <li className="menu_item">
+              <div className="menu_wrapper">
+                <Link 
+                  href="/" 
+                  className={`flex items-center gap-2.5 pl-2.5 pt-3 pb-3 pr-2.5 text-base ${
+                    pathname === "/" ? "navActive" : ""
+                  }`}
+                >
                   <GoHome className="w-5 h-5" />
                   Dashboards
                   <IoIosArrowForward className="ml-auto" />
                 </Link>
               </div>
             </li>
-            <li className={`menu_item`}>
-              <div className="menu_wrapper pl-2.5 pt-3 pb-3 pr-2.5">
-                <Link href={"/ecommerce"} className="flex items-center gap-2.5 text-base" onClick={handleactiveClick}>
+            <li className="menu_item">
+              <div className="menu_wrapper">
+                <Link 
+                  href="/ecommerce" 
+                  className={`flex items-center gap-2.5 pl-2.5 pt-3 pb-3 pr-2.5 text-base ${
+                    pathname === "/ecommerce" ? "navActive" : ""
+                  }`}
+                >
                   <HiOutlineShoppingBag className="w-5 h-5" />
                   Ecommerce
                   <IoIosArrowForward className="ml-auto" />
@@ -36,26 +44,6 @@ const Sidebar = () => {
             </li>
           </ul>
           <h4 className="text-sm pt-4 pl-1.5 pb-2.5 uppercase">Application</h4>
-          <ul>
-            <li className={`menu_item`}>
-              <div className="menu_wrapper pl-2.5 pt-3 pb-3 pr-2.5">
-                <Link href={"/"} className="flex items-center gap-2.5 text-base" onClick={handleactiveClick}>
-                  <GoHome className="w-5 h-5" />
-                  Dashboards
-                  <IoIosArrowForward className="ml-auto" />
-                </Link>
-              </div>
-            </li>
-            <li className={`menu_item`}>
-              <div className="menu_wrapper pl-2.5 pt-3 pb-3 pr-2.5">
-                <Link href={"/"} className="flex items-center gap-2.5 text-base" onClick={handleactiveClick}>
-                  <HiOutlineShoppingBag className="w-5 h-5" />
-                  Ecommerce
-                  <IoIosArrowForward className="ml-auto" />
-                </Link>
-              </div>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
